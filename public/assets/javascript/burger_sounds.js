1af4825c80e1a63eadfd92036774cdf65acceff9
shadowlaq <escamillaa@email.arizona.edu>
2018-08-08 18:48:11 -0700

    fix json

--- public/assets/javascript/burger_sounds.js
+++ public/assets/javascript/burger_sounds.js
@@ -6,25 +6,23 @@
 $(document).ready(function() {
 
   // Pre-load sound effects
-  var audioWhine0 = new Audio('http://soundbible.com/mp3/TomCat-Mr_Smith-2055290520.mp3');
-  var audioWhine1 = new Audio('http://soundbible.com/mp3/Sheep Bleating-SoundBible.com-1373580685.mp3');
-  var audioWhine2 = new Audio('http://soundbible.com/mp3/Turkey Gobble-SoundBible.com-312252853.mp3');
-  var audioWhine3 = new Audio('http://soundbible.com/mp3/Dinosaur Roar-SoundBible.com-605392672.mp3');
-  var audioWhine4 = new Audio('http://soundbible.com/mp3/Lion-SoundBible.com-621763115.mp3');
+
+  var animalSounds = {
+    1:new Audio('http://soundbible.com/mp3/TomCat-Mr_Smith-2055290520.mp3'),
+    2:new Audio('http://soundbible.com/mp3/Sheep Bleating-SoundBible.com-1373580685.mp3'),
+    3:new Audio('http://soundbible.com/mp3/Turkey Gobble-SoundBible.com-312252853.mp3'),
+    4:new Audio('http://soundbible.com/mp3/Lion-SoundBible.com-621763115.mp3')
+  };
 
 
-  // Array of sounds
-  var soundArray = [audioWhine0, audioWhine1, audioWhine2, audioWhine3, audioWhine4];
-
-
-  // Click Listener for Whine Button
-  $('.whine-button').on("click", function(){
+  // Click Listener for growl Button
+  $('.growl-button').on("click", function(){
 
     // Generate a random number (from 0 to length of array)
-    var i = Math.floor(Math.random()*soundArray.length);
+    var i = Math.floor(Math.random()*3);
 
     // Play the random sound
-    soundArray[i].play();
+    animalSounds[i].play();
 
   });
 
