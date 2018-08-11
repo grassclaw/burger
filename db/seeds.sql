50916ad584f8ec52bf645727b0a7918358ee1c19
shadowlaq <escamillaa@email.arizona.edu>
2018-08-11 11:51:17 -0700

    json

--- db/seeds.sql
+++ db/seeds.sql
@@ -7,3 +7,35 @@ VALUES ("Big Mac", false, "2018-08-05 05:10:11"),
   ("Tri Pounder", false, "2016-08-05 18:20:01"),
   ("FancyBurger", false, "2018-08-05 04:20:20"),
   ("Triple Single?", true, "2018-08-05 18:22:11");
+
+
+
+
+  -- Database Creation
+-- CREATE DATABASE burgers_db;
+
+USE burgers_db;
+
+
+-- Table Creation
+CREATE TABLE burgers(
+	id INTEGER AUTO_INCREMENT PRIMARY KEY,
+	burger_name VARCHAR(50),
+	devoured BOOLEAN Default false,
+	createdAt TIMESTAMP NOT NULL Default CURRENT_TIMESTAMP);
+
+-- Seed Burgers into Database ... No Sesame! (hehehe)
+INSERT INTO burgers(burger_name)
+VALUES ("Big Mac"),
+  ("Whopping Whopper"),
+  ("CheesyBurga!"),
+  ("WakinBacon"),
+  ("Tri Pounder"),
+  ("FancyBurger"),
+  ("Triple Single?");
+  
+  select * from burgers;
+  
+  USE burgers_db;
+
+  Drop Table if exists burgers;
--- server.js
+++ server.js
@@ -16,8 +16,6 @@ var exphbs = require('express-handlebars');
 app.engine('handlebars', exphbs({defaultLayout: 'main'}));
 app.set('view engine', 'handlebars');
 
-
-
 var routes = require("./controllers/burgers_controller.js");
 
 app.use(routes);
